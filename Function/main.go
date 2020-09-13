c0ea7d6dcc9a7a9ea0ef41f840be49d7ea43134f
JF <github@itgeek.men>
2020-09-13 22:15:41 +0300

    init

--- Function/main.go
+++ Function/main.go
@@ -1,24 +1,24 @@
 package main
 import (
-	"fmt"
+	//"fmt"
 	"github.com/docker/distribution/registry"
 	dcontext "github.com/docker/distribution/context"
 	"github.com/docker/distribution/version"
 	"github.com/docker/distribution/configuration"
 	_ "github.com/docker/distribution/registry/storage/driver/azure"
 	"bytes"
+    "os"
 )
 
-const strgAccName string = ""
-const strgAccKey string = ""
-const strgContainer string = ""
 
-var dYAML string = ("version: 0.1\nstorage:\n  azure:\n    accountname: " + strgAccName + "\n    accountkey: " + strgAccKey + "\n    container: " + strgContainer + "\nhttp:\n  addr: :5000")
 
 func main(){
-	fmt.Println(dYAML)
+    strgAccName,_ := os.LookupEnv("FUNCTIONS_STORAGE_NAME")
+    strgAccKey,_ := os.LookupEnv("FUNCTIONS_STORAGE_KEY")
+    strgContainer,_ := os.LookupEnv("FUNCTIONS_STORAGE_CONTAINER")
+    var dYAML string = ("version: 0.1\nstorage:\n  azure:\n    accountname: " + strgAccName + "\n    accountkey: " + strgAccKey + "\n    container: " + strgContainer + "\nhttp:\n  addr: :5000")
 	config, _ := configuration.Parse(bytes.NewReader([]byte(dYAML)))
 	ctx := dcontext.WithVersion(dcontext.Background(), version.Version)
 	r, _ := registry.NewRegistry(ctx, config)
-	fmt.Println(dYAML,r)
+    r.ListenAndServe()
 }
---
+++ Script/main.go
@@ -0,0 +1,62 @@
+package main
+
+
+import (
+    "fmt"
+    "golang.org/x/crypto/ssh/terminal"
+    "golang.org/x/crypto/bcrypt"
+    "os"
+    "bufio"
+    "syscall"
+    "strings"
+    "log"
+)
+
+func HashPassword(password string) (string, error) {
+    bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+    return string(bytes), err
+}
+
+func CheckPasswordHash(password, hash string) bool {
+    err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+    return err == nil
+}
+
+func credentials() (string, string) {
+    reader := bufio.NewReader(os.Stdin)
+
+    fmt.Print("Enter Username: ")
+    username, err := reader.ReadString('\n')
+
+    if err != nil || len(username) <= 1 {
+        log.Fatal("Incorrect Username")
+    }
+    
+    fmt.Print("Enter Password: ")
+    bytePassword, err := terminal.ReadPassword(int(syscall.Stdin))
+    if err != nil || bytePassword == nil {
+        log.Fatal("Incorrect Password") 
+    }
+    fmt.Println("")
+    password := string(bytePassword)
+
+    return strings.TrimSpace(username), strings.TrimSpace(password)
+}
+
+func main() {
+    username, password := credentials()
+    hash, err := HashPassword(password) 
+    if err != nil {                                                                    
+        log.Fatal("Couldn't generate a hash")                                                                       
+    } 
+    htpasswd, err := os.Create("htpasswd")
+    if err != nil {                                                                                           
+        log.Fatal("Couldn't access htpasswd file")                                                                 
+    }
+    defer htpasswd.Close()
+    
+    htpasswd.WriteString(username + ":" +hash)
+    htpasswd.Sync()
+
+    fmt.Println("\n[Done] htpasswd was successfully generate")
+}
