04399c0c0faef6a562e2a2a2735b190b1de82c4f
JF <github@itgeek.men>
2020-09-17 00:20:08 +0300

    working on mount path

--- run.sh
+++ run.sh
@@ -7,7 +7,15 @@ grpName="registry-group"
 grpRegion="westeurope"
 strgConfig="config"
 strgData="data"
+authConfig='htpasswd'
+
 
 #az deployment sub create --location $grpRegion --template-uri https://raw.githubusercontent.com/groovy-sky/self-registry/master/Template/group.json --parameters Location=$grpRegion Name=$grpName
 
-az deployment group create --resource-group $grpName --template-uri https://raw.githubusercontent.com/groovy-sky/self-registry/master/Template/storage.json --parameters containerConfigName=$strgConfig containerDataName=$strgData
+strgName=$(az deployment group create --resource-group $grpName --template-uri https://raw.githubusercontent.com/groovy-sky/self-registry/master/Template/storage.json --parameters containerConfigName=$strgConfig containerDataName=$strgData | jq -r '.properties.outputs | map(.value) | add' )
+
+strgKey=$(az storage account keys list --account-name $strgName | jq -r '. | first.value')
+
+az storage blob upload --account-name $strgName --account-key $strgKey --container-name $strgConfig --name $authConfig --file $authConfig
+
+#https://docs.microsoft.com/en-us/cli/azure/webapp/config/storage-account?view=azure-cli-latest
