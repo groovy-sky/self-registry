6628a62eb4b99c2b38b8304a16c7fe200b4a73c9
JF <github@itgeek.men>
2020-09-16 22:04:02 +0300

    working on storage

--- run.sh
+++ run.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-go get -u golang.org/x/crypto/bcrypt
-go get -u golang.org/x/crypto/ssh/terminal
+#go get -u golang.org/x/crypto/bcrypt
+#go get -u golang.org/x/crypto/ssh/terminal
+#go run Script/main.go
 
 grpName="registry-group"
 grpRegion="westeurope"
 strgConfig="config"
 strgData="data"
 
-#go run Script/main.go
+#az deployment sub create --location $grpRegion --template-uri https://raw.githubusercontent.com/groovy-sky/self-registry/master/Template/group.json --parameters Location=$grpRegion Name=$grpName
 
-
+az deployment group create --resource-group $grpName --template-uri https://raw.githubusercontent.com/groovy-sky/self-registry/master/Template/storage.json --parameters containerConfigName=$strgConfig containerDataName=$strgData
