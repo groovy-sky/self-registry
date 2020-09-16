43552e01c29a4260907700c47d5bcdc8675278d7
JF <github@itgeek.men>
2020-09-16 21:39:11 +0300

    resources initial deployment

--- run.sh
+++ run.sh
@@ -1,2 +1,12 @@
+#!/bin/bash
 go get -u golang.org/x/crypto/bcrypt
 go get -u golang.org/x/crypto/ssh/terminal
+
+grpName="registry-group"
+grpRegion="westeurope"
+strgConfig="config"
+strgData="data"
+
+#go run Script/main.go
+
+
